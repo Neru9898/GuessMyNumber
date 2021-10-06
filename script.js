e4a9100edbc190b349ff46f656d6c267b3987f94
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-10-05 20:16:51 -0400

    Worked on High Score function

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ let numToGuess = Math.floor(Math.random() * 20) + 1;
 let highScore = 0;
 const checkNum = () => {
   const inputNum = Number(document.querySelector(".guess").value);
+  const score = document.querySelector(".score").innerHTML;
 
   if (inputNum >= 1 && inputNum <= 20) {
     if (inputNum === numToGuess) {
       document.querySelector(
         ".message"
       ).innerHTML = `You guessed correct!!✅✅`;
+      score > highScore
+        ? (document.querySelector(".highscore").innerHTML = score)
+        : null;
     } else {
-      document.querySelector(
-        ".message"
-      ).innerHTML = `You guessed incorrect!!❌❌`;
+      inputNum > numToGuess
+        ? (document.querySelector(
+            ".message"
+          ).innerHTML = `You guessed incorrect!! Your guess is to high!!❌❌`)
+        : (document.querySelector(
+            ".message"
+          ).innerHTML = `You guessed incorrect!! Your guess is to low!!❌❌`);
       document.querySelector(".score").innerHTML -= 1;
     }
   } else {
@@ -20,5 +28,4 @@ const checkNum = () => {
       ".message"
     ).innerHTML = `${inputNum} not in range!!! Pick between 1 and 20`;
   }
-  console.log(numToGuess);
 };
